ffd4df6d2e4bc6d90203eecc207f67313baf82cd
PratikT112 <pratiktelang112@gmail.com>
2024-12-22 23:30:08 +0530

    normal commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 
 const Header = () => {
@@ -8,10 +9,12 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="ProShop Logo" />
-            ProShop
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="ProShop Logo" />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
@@ -19,6 +22,7 @@ const Header = () => {
                 <FaShoppingCart />
                 Cart
               </Nav.Link>
+
               <Nav.Link href="/login">
                 <FaUser />
                 Sign In
